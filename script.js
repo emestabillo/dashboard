d0aa9a2a85fc2e1f56e84f0d1fd3b445937c341c
Emmilie Estabillo <emsestabillo@gmail.com>
2020-04-28 13:43:42 -0400

    Section scss

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const cards = Array.from(document.querySelectorAll(".card"));
 const toggle = document.querySelector(".switch");
 const slider = document.querySelector(".slider");
 const body = document.querySelector("body");
-const overview = document.querySelector(".overview__title");
+// const overview = document.querySelector(".overview__title");
+const ary = Array.from(document.querySelectorAll("h5, p"));
 
 toggle.addEventListener("change", changeTheme);
 
@@ -12,7 +13,10 @@ function changeTheme() {
 
     slider.classList.toggle("light-slider");
 
-    overview.classList.toggle("gray-text");
+    // overview.classList.toggle("gray-text");
+    for (var i = 0; i < ary.length; i++) {
+      ary[i].classList.toggle("gray-text");
+    }
 
     for (var i = 0; i < cards.length; i++) {
       cards[i].classList.toggle("light-card");
