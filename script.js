878f191c45768edb85d04af16b914b99bc1a8c89
Emmilie Estabillo <emsestabillo@gmail.com>
2020-04-28 07:42:39 -0400

    Edit styles for light theme

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const cards = Array.from(document.querySelectorAll(".card"));
 const toggle = document.querySelector(".switch");
 const slider = document.querySelector(".slider");
 const body = document.querySelector("body");
+const overview = document.querySelector(".overview__title");
 
 toggle.addEventListener("change", changeTheme);
 
@@ -10,6 +11,8 @@ function changeTheme() {
     body.classList.toggle("light-bg");
 
     slider.classList.toggle("light-slider");
+
+    overview.classList.toggle("gray-text");
 
     for (var i = 0; i < cards.length; i++) {
       cards[i].classList.toggle("light-card");
